15440791a33e2ef9f4ee21dce25137fcbf5e3e70
Sajid Hasan Arafat <116008772+sajidhasanarafat@users.noreply.github.com>
2023-09-09 03:31:29 -0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,33 @@
+
+
+$(function(){
+    $('.bars').click(function(){
+        $('.menu_wrapper').toggleClass('open_menu');
+        $('.menu_overlay').toggle()
+
+    })
+
+    $('.back_to_top').click(function(){
+        $('html, body').animate({scrollTop: 0}, 1000)
+    })
+
+    $(window).scroll(function(){
+        var scrollTop = $(this).scrollTop();
+
+        if(scrollTop > 500){
+            $('.back_to_top').show(500);
+            $('.menu_wrapper').removeClass('open_menu');
+        $('.menu_overlay').hide();
+        }else{
+            $('.back_to_top').hide(500);
+        }
+    })
+
+
+
+    $('.menu_overlay').click(function(){
+        $('.menu_wrapper').removeClass('open_menu');
+        $('.menu_overlay').toggle();
+    })
+
+})
